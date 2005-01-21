cf18a1322fcbe7e84ff1e618adf3788e5e95af0c
Trey Smith <trey.smith@gmail.com>
2005-01-21 16:26:04 +0000

    initial check-in
    git-svn-id: file:///Users/mfsmith3/sandbox/cvs2svn/svnrepo/trunk/src@31 3bbcab3b-a713-480a-a40a-1fdaa183d0c8

---
+++ common/ublasMatrixTypes.h
@@ -0,0 +1,70 @@
+/********** tell emacs we use -*- c++ -*- style comments *******************
+ * $Revision: 1.1 $  $Author: trey $  $Date: 2005-01-21 16:26:04 $
+ *  
+ * @file    ublasMatrixTypes.h
+ * @brief   No brief
+ ***************************************************************************/
+
+#ifndef INCublasMatrixTypes_h
+#define INCublasMatrixTypes_h
+
+// if VEC_OPTIM is set (check the makefile), set NDEBUG just for the vector/matrix headers
+#if VEC_OPTIM
+#  ifdef NDEBUG
+#     define VEC_NDEBUG_WAS_DEFINED 1
+#  else
+#     define NDEBUG 1
+#  endif
+#endif
+
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/operation.hpp>
+#include <boost/numeric/ublas/vector_sparse.hpp>
+#include <boost/numeric/ublas/matrix_sparse.hpp>
+
+// for some reason, this needs to be included after the others
+#include <boost/numeric/ublas/io.hpp>
+
+typedef boost::numeric::ublas::vector<double> dvector;
+typedef boost::numeric::ublas::matrix<double> dmatrix;
+
+typedef boost::numeric::ublas::compressed_vector<double> cvector;
+typedef boost::numeric::ublas::compressed_matrix<double> cmatrix;
+
+#if NO_COMPRESSED_MATRICES
+
+/* dense vector/matrix implementation */
+#define VEC_ASSIGN_CHECK_ZERO(x,y) ; { (x) = (y); }
+
+typedef dvector bvector;
+typedef dmatrix bmatrix;
+
+#else
+
+/* compressed vector/matrix implementation */
+#define VEC_ASSIGN_CHECK_ZERO(x,y) \
+  ; { typeof(y) __tmp_y = (y); if (__tmp_y != 0) { (x) = __tmp_y; } }
+
+typedef cvector bvector;
+typedef cmatrix bmatrix;
+
+#endif
+
+// select storage types of the data structures we use
+typedef cvector belief_vector;
+typedef dvector alpha_vector;
+typedef dvector obs_prob_vector;
+
+// undefine NDEBUG if it was previously undefined
+#if VEC_OPTIM && !VEC_NDEBUG_WAS_DEFINED
+#   undef NDEBUG
+#endif
+
+#endif // INCublasMatrixTypes_h
+
+/***************************************************************************
+ * REVISION HISTORY:
+ * $Log: not supported by cvs2svn $
+ *
+ ***************************************************************************/
