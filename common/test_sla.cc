4e9c2f0a38013e453f7d34c8d2b4261669b75b08
Trey Smith <trey.smith@gmail.com>
2005-01-27 05:34:33 +0000

    added lots of regression tests
    git-svn-id: file:///Users/mfsmith3/sandbox/cvs2svn/svnrepo/trunk/src@53 3bbcab3b-a713-480a-a40a-1fdaa183d0c8

--- common/test_sla.cc
+++ common/test_sla.cc
@@ -4,34 +4,352 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 #include "sla.h"
 
 using namespace std;
 using namespace sla;
 
-void usage(void) {
-  cerr <<
-    "usage: test_sla\n"
-    "  -h or --help   Print this help\n";
-  exit(-1);
+void test_dvector(void) {
+  dvector xd(3);
+  xd.resize(4);
+  xd(0) = 1;
+  xd(1) = 2;
+  xd(2) = 3;
+  xd(3) = 4;
+  cout << "--xd: size=4 data=1 2 3 4" << endl;
+  cout << "  xd: size=" << xd.size()
+       << " data=" << xd(0) << " " << xd(1)
+       << " " << xd(2) << " " << xd(3) << endl;
+  
+  xd *= 2.0;
+  cout << "--xd: data=2 4 6 8" << endl;
+  cout << "  xd: data=" << xd(0) << " " << xd(1)
+       << " " << xd(2) << " " << xd(3) << endl;
+
+  dvector yd(4);
+  yd(0) = 5;
+  yd(1) = 4;
+  yd(2) = 3;
+  yd(3) = 2;
+
+  xd += yd;
+  cout << "--xd: data=7 8 9 10" << endl;
+  cout << "  xd: data=" << xd(0) << " " << xd(1)
+       << " " << xd(2) << " " << xd(3) << endl;
+
+  string xs = "2 9 7";
+  istringstream iss(xs);
+  xd.read(iss);
+  cout << "--xd: size=2 data=9 7" << endl;
+  cout << "  xd: size=" << xd.size()
+       << " data=" << xd(0) << " " << xd(1) << endl;
 }
 
-void test_correctness(void)
-{
-  cmatrix A;
-  cvector x, result;
-  dvector x_in, result_out;
+void test_cvector(void) {
+  dvector xd(3);
+  xd.resize(4);
+  xd(0) = 1;
+  xd(1) = 2;
+  xd(2) = 3;
+  xd(3) = 4;
 
-  read_from_file(A,"A.dat");
-  A.write(cout);
-  read_from_file(x_in,"x.dat");
-  cvector_from_dvector(x, x_in);
-  result.resize(x.size());
-  mult( result, A, x );
+  cvector xc;
+  cvector_from_dvector( xc, xd );
+
+  cout << "--xc: size=4 data=1 2 3 4" << endl;
+  cout << "  xc: size=" << xc.size()
+       << " data=" << xc(0) << " " << xc(1)
+       << " " << xc(2) << " " << xc(3) << endl;
   
-  dvector_from_cvector(result_out, result);
-  result_out.write(cout);
+  xc *= 2;
+  cout << "--xc: data=2 4 6 8" << endl;
+  cout << "  xc: data=" << xc(0) << " " << xc(1)
+       << " " << xc(2) << " " << xc(3) << endl;
+
+  // resize() implicitly sets everything to zero
+  xc.resize(5);
+  // push_back() calls must be in increasing order of index.
+  xc.push_back(1,-1);
+  xc.push_back(3,17);
+  xc.canonicalize();
+  cout << "--xc: size=5 data=0 -1 0 17 0" << endl;
+  cout << "--xc: size=" << xc.size()
+       << " data=" << xc(0) << " " << xc(1)
+       << " " << xc(2) << " " << xc(3) << " " << xc(4) << endl;
+
+  string xs = "4 " "2 " "0 7 " "3 9 ";
+  istringstream iss(xs);
+  xc.read(iss);
+  cout << "--xc: size=4 data=7 0 0 9" << endl;
+  cout << "  xc: size=" << xc.size()
+       << " data=" << xc(0) << " " << xc(1)
+       << " " << xc(2) << " " << xc(3) << endl;
+}
+
+void test_dmatrix(void) {
+  dmatrix Ad;
+  Ad.resize(2,2);
+  Ad(0,0) = 1;
+  Ad(0,1) = 2;
+  Ad(1,0) = 3;
+  Ad(1,1) = 4;
+
+  cout << "--Ad: size=2 2 data=1 2 3 4" << endl;
+  cout << "  Ad: size=" << Ad.size1() << " " << Ad.size2()
+       << " data=" << Ad(0,0) << " " << Ad(0,1)
+       << " " << Ad(1,0) << " " << Ad(1,1) << endl;
+}
+
+void test_kmatrix(void) {
+  kmatrix Ak;
+  Ak.resize(3,3);
+
+  cout << "--Ak: size=3 3 filled=0" << endl;
+  cout << "  Ak: size=" << Ak.size1() << " " << Ak.size2()
+       << " filled=" << Ak.filled() << endl;
+  
+  // resize implicitly sets everything to zero.
+  Ak.resize(2,2);
+  // push_back calls need not be in order.  among calls with the same
+  // (r,c) value the last one will be retained.  kmatrix_set_entry()
+  // is an alias for kmatrix::push_back
+  Ak.push_back(0,0,6);
+  Ak.push_back(1,1,2);
+  Ak.push_back(1,0,3);
+  Ak.push_back(1,1,9);
+  // must call canonicalize before doing anything else with the matrix
+  Ak.canonicalize();
+
+  cout << "--Ak: size=2 2 filled=3 data=6 0 3 9" << endl;
+  cout << "  Ak: size=" << Ak.size1() << " " << Ak.size2()
+       << " filled=" << Ak.filled()
+       << " data=" << Ak(0,0) << " " << Ak(0,1)
+       << " " << Ak(1,0) << " " << Ak(1,1) << endl;
+
+  string As = "2 2 " "2 " "0 1 5 " "1 0 7 ";
+  istringstream iss(As);
+  Ak.read(iss);
+  Ak.canonicalize();
+  cout << "--Ak: size=2 2 filled=2 data=0 5 7 0" << endl;
+  cout << "  Ak: size=" << Ak.size1() << " " << Ak.size2()
+       << " filled=" << Ak.filled()
+       << " data=" << Ak(0,0) << " " << Ak(0,1)
+       << " " << Ak(1,0) << " " << Ak(1,1) << endl;
+}
+
+void test_cmatrix(void) {
+  cmatrix Ac;
+  Ac.resize(3,3);
+
+  cout << "--Ac: size=3 3 filled=0" << endl;
+  cout << "  Ac: size=" << Ac.size1() << " " << Ac.size2()
+       << " filled=" << Ac.filled() << endl;
+
+  // resize implicitly sets everything to zero.
+  Ac.resize(2,2);
+  // push_back calls must be in column-major order.
+  Ac.push_back(0,0,6);
+  Ac.push_back(1,0,3);
+  Ac.push_back(1,1,9);
+  // must call canonicalize before doing anything else with the matrix
+  Ac.canonicalize();
+
+  cout << "--Ac: size=2 2 filled=3 data=6 0 3 9" << endl;
+  cout << "  Ac: size=" << Ac.size1() << " " << Ac.size2()
+       << " filled=" << Ac.filled()
+       << " data=" << Ac(0,0) << " " << Ac(0,1)
+       << " " << Ac(1,0) << " " << Ac(1,1) << endl;
+
+  string As = "2 2 " "2 " "0 1 5 " "1 0 7 ";
+  istringstream iss(As);
+  Ac.read(iss);
+  cout << "--Ac: size=2 2 filled=2 data=0 5 7 0" << endl;
+  cout << "  Ac: size=" << Ac.size1() << " " << Ac.size2()
+       << " filled=" << Ac.filled()
+       << " data=" << Ac(0,0) << " " << Ac(0,1)
+       << " " << Ac(1,0) << " " << Ac(1,1) << endl;
+}
+
+void test_conversions(void)
+{
+  dvector xd, xd2;
+  cvector xc;
+  kmatrix Ak;
+  cmatrix Ac;
+
+  xd.resize(3);
+  xd(0) = 2;
+  xd(1) = 0;
+  xd(2) = 6;
+
+  cvector_from_dvector( xc, xd );
+
+  cout << "--xc: size=3 filled=2 data=2 0 6" << endl;
+  cout << "  xc: size=" << xc.size()
+       << " filled=" << xc.filled()
+       << " data=" << xc(0) << " " << xc(1) << " " << xc(2) << endl;
+
+  dvector_from_cvector( xd2, xc );
+  
+  cout << "--xd2: size=3 data=2 0 6" << endl;
+  cout << "  xd2: size=" << xd2.size()
+       << " data=" << xd2(0) << " " << xd2(1) << " " << xd2(2) << endl;
+
+
+  string As = "2 2 " "2 " "0 1 5 " "1 0 7 ";
+  istringstream iss(As);
+  Ak.read(iss);
+  Ak.canonicalize();
+  
+  cmatrix_from_kmatrix( Ac, Ak );
+
+  cout << "--Ac: size=2 2 filled=2 data=0 5 7 0" << endl;
+  cout << "  Ac: size=" << Ac.size1() << " " << Ac.size2()
+       << " filled=" << Ac.filled()
+       << " data=" << Ac(0,0) << " " << Ac(0,1)
+       << " " << Ac(1,0) << " " << Ac(1,1) << endl;
+
+  cvector_from_cmatrix_column( xc, Ac, 0 );
+
+  cout << "--xc: size=2 filled=1 data=0 7" << endl;
+  cout << "  xc: size=" << xc.size()
+       << " filled=" << xc.filled()
+       << " data=" << xc(0) << " " << xc(1) << endl;
+
+  cvector_from_cmatrix_column( xc, Ac, 1 );
+
+  cout << "--xc: size=2 filled=1 data=5 0" << endl;
+  cout << "  xc: size=" << xc.size()
+       << " filled=" << xc.filled()
+       << " data=" << xc(0) << " " << xc(1) << endl;
+
+  // resize implicitly sets everything to zero.
+  Ak.resize(2,2);
+  // kmatrix_set_entry() calls need not be in order.
+  kmatrix_set_entry( Ak, 0, 0, 6 );
+  kmatrix_set_entry( Ak, 1, 1, 2 );
+  kmatrix_set_entry( Ak, 1, 0, 3 );
+  kmatrix_set_entry( Ak, 1, 1, 9 );
+  // must call canonicalize before doing anything else with the matrix
+  Ak.canonicalize();
+
+  cout << "--Ak: size=2 2 filled=3 data=6 0 3 9" << endl;
+  cout << "  Ak: size=" << Ak.size1() << " " << Ak.size2()
+       << " filled=" << Ak.filled()
+       << " data=" << Ak(0,0) << " " << Ak(0,1)
+       << " " << Ak(1,0) << " " << Ak(1,1) << endl;
+
+  kmatrix_transpose_in_place( Ak );
+  cout << "--Ak: size=2 2 filled=3 data=6 3 0 9" << endl;
+  cout << "  Ak: size=" << Ak.size1() << " " << Ak.size2()
+       << " filled=" << Ak.filled()
+       << " data=" << Ak(0,0) << " " << Ak(0,1)
+       << " " << Ak(1,0) << " " << Ak(1,1) << endl;
+}
+
+void test_unary(void)
+{
+  cvector xc;
+  double s;
+  dvector xd;
+
+  istringstream iss("5 " "3 " "1 2 " "3 5 " "4 -3 ");
+  xc.read(iss);
+  s = norm_1(xc);
+
+  cout << "--s=10" << endl;
+  cout << "  s=" << s << endl;
+
+  istringstream iss2("5 " "2 " "3 6 " "4 -1 ");
+  xc.read(iss2);
+  s = norm_1(xc);
+
+  cout << "--s=7" << endl;
+  cout << "  s=" << s << endl;
+
+  istringstream iss3("5 " "0 2 0 5 -3");
+  xd.read(iss3);
+  s = norm_inf(xd);
+
+  cout << "--s=5" << endl;
+  cout << "  s=" << s << endl;
+
+  // reusing iss again causes a weird error...
+  // but valgrind is happy, so i won't look into it further
+  istringstream iss4("5 " "0 2 0 5 -7");
+  xd.read(iss4);
+  s = norm_inf(xd);
+
+  cout << "--s=7" << endl;
+  cout << "  s=" << s << endl;
+}
+
+void test_binary(void)
+{
+  cvector xc, yc, zc;
+  dvector xd, yd;
+  cmatrix Ac;
+  double s;
+
+  istringstream iss("2 2 " "3 " "0 0 2 " "0 1 5 " "1 0 3 ");
+  Ac.read(iss);
+
+  istringstream iss2("2 " "2 " "0 7 " "1 11 ");
+  xc.read(iss2);
+
+  istringstream iss3("2 " "7 11 ");
+  xd.read(iss3);
+
+  mult( yd, Ac, xc );
+
+  cout << "--yd: 69 21" << endl;
+  cout << "  yd: " << yd(0) << " " << yd(1) << endl;
+
+  mult( yc, Ac, xc );
+
+  cout << "--yc: 69 21" << endl;
+  cout << "  yc: " << yc(0) << " " << yc(1) << endl;
+
+  mult( yd, xd, Ac );
+
+  cout << "--yd: 47 35" << endl;
+  cout << "  yd: " << yd(0) << " " << yd(1) << endl;
+
+  emult( zc, xc, yc );
+
+  cout << "--zc: 483 231" << endl;
+  cout << "  zc: " << zc(0) << " " << zc(1) << endl;
+
+  istringstream iss4("7 " "4 " "1 2 " "2 3 " "4 5 " "5 7 ");
+  xc.read(iss4);
+
+  istringstream iss5("7 " "4 " "2 11 " "3 13 " "5 17 " "6 19 ");
+  yc.read(iss5);
+
+  emult( zc, xc, yc );
+  
+  cout << "--zc: 0 0 33 0 0 119 0" << endl;
+  cout << "  zc: " << zc(0) << " " << zc(1)
+       << " " << zc(2) << " " << zc(3)
+       << " " << zc(4) << " " << zc(5)
+       << " " << zc(6) << endl;
+
+  emult( zc, yc, xc );
+
+  cout << "--zc: 0 0 33 0 0 119 0" << endl;
+  cout << "  zc: " << zc(0) << " " << zc(1)
+       << " " << zc(2) << " " << zc(3)
+       << " " << zc(4) << " " << zc(5)
+       << " " << zc(6) << endl;
+
+  cvector_from_dvector( xc, xd );
+
+  s = inner_prod( xd, xc );
+  
+  cout << "--s: 170" << endl;
+  cout << "  s: " << s << endl;
 }
 
 void test_performance(void)
@@ -72,8 +390,14 @@ void test_performance(void)
   write_to_file(result_out,"result.dat");
 }
 
-int main(int argc, char **argv) {
+void usage(void) {
+  cerr <<
+    "usage: test_sla\n"
+    "  -h or --help   Print this help\n";
+  exit(-1);
+}
 
+int main(int argc, char** argv) {
   for (int argi=1; argi < argc; argi++) {
     string args = argv[argi];
     if (args[0] == '-') {
@@ -89,7 +413,15 @@ int main(int argc, char **argv) {
     }
   }
 
-  //test_correctness();
+  test_dvector();
+  test_cvector();
+  test_dmatrix();
+  test_kmatrix();
+  test_cmatrix();
+  test_conversions();
+  test_unary();
+  test_binary();
+
   test_performance();
 
   return 0;
