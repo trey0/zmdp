b88f7889a1fd8e72ca6c10c2e7c1983cb3ffa48d
Trey Smith <trey.smith@gmail.com>
2005-10-27 21:27:10 +0000

    renamed pomdp to pomdpCassandraWrapper
    git-svn-id: file:///Users/mfsmith3/sandbox/cvs2svn/svnrepo/trunk/src@125 3bbcab3b-a713-480a-a40a-1fdaa183d0c8

--- parsers/pomdpCassandraWrapper.cc
+++ parsers/pomdpCassandraWrapper.cc
@@ -1,10 +1,14 @@
 /********** tell emacs we use -*- c++ -*- style comments *******************
- * $Revision: 1.1.1.1 $  $Author: trey $  $Date: 2004-11-09 16:18:56 $
+ * $Revision: 1.1 $  $Author: trey $  $Date: 2005-10-27 21:27:10 $
  *  
  * PROJECT: FIRE Architecture Project
  *
- * @file    template.cc
- * @brief   No brief
+ * @file    pomdpCassandraWrapper.cc
+ * @brief   A wrapper that provides access to the pomdp read in by
+ *          Tony Cassandra's file reading code, without requiring you
+ *          to (1) directly reference global variables or (2) include
+ *          all of the headers like sparse-matrix.h.  Also uses notation
+ *          I am more familiar with.
  ***************************************************************************/
 
 /***************************************************************************
@@ -19,7 +23,7 @@
 #include <iostream>
 using namespace std;
 
-#include "pomdp.h"
+#include "pomdpCassandraWrapper.h"
 #include "mdp.h"
 
 int Pomdp::getNumStates(void) const {
@@ -67,6 +71,9 @@ void Pomdp::readFromFile(const string& fileName) {
 /***************************************************************************
  * REVISION HISTORY:
  * $Log: not supported by cvs2svn $
+ * Revision 1.1.1.1  2004/11/09 16:18:56  trey
+ * imported hsvi into new repository
+ *
  * Revision 1.3  2003/07/16 16:12:44  trey
  * added support for terminal states
  *
--- parsers/pomdpCassandraWrapper.h
+++ parsers/pomdpCassandraWrapper.h
@@ -1,14 +1,16 @@
 /********** tell emacs we use -*- c++ -*- style comments *******************
- * $Revision: 1.1.1.1 $  $Author: trey $  $Date: 2004-11-09 16:18:56 $
+ * $Revision: 1.1 $  $Author: trey $  $Date: 2005-10-27 21:27:10 $
  *  
- * PROJECT: FIRE Architecture Project
- *
- * @file    spec.h
- * @brief   No brief
+ * @file    pomdpCassandraWrapper.h
+ * @brief   A wrapper that provides access to the pomdp read in by
+ *          Tony Cassandra's file reading code, without requiring you
+ *          to (1) directly reference global variables or (2) include
+ *          all of the headers like sparse-matrix.h.  Also uses notation
+ *          I am more familiar with.
  ***************************************************************************/
 
-#ifndef INCspec_h
-#define INCspec_h
+#ifndef INCpomdpCassandraWrapper_h
+#define INCpomdpCassandraWrapper_h
 
 using namespace std;
 
@@ -23,8 +25,8 @@ typedef int ObsType;
 typedef double ProbType;
 typedef double ValueType;
 
-struct Pomdp {
-  Pomdp(void) {}
+struct PomdpCassandraWrapper {
+  PomdpCassandraWrapper(void) {}
 
   int getNumStates(void) const;
   int getNumActions(void) const;
@@ -33,24 +35,27 @@ struct Pomdp {
   ValueType getInitialBelief(StateType s) const;
   bool isTerminalState(StateType s) const;
 
+  // rewards
   ValueType R(StateType s, ActionType a) const;
+
+  // transition probabilities
   ProbType T(StateType s, ActionType a, StateType sp) const;
+
+  // observation probabilities
   ProbType O(StateType sp, ActionType a, ObsType o) const;
 
-#ifdef SWIG
-  // SWIG only understands char * as equivalent to python strings
-  void readFromFile(const char *fileName) { readFromFile(string(fileName)); }
-#else
   void readFromFile(const string& fileName);
-#endif
 };
 
 
-#endif // INCspec_h
+#endif // INCpomdpCassandraWrapper_h
 
 /***************************************************************************
  * REVISION HISTORY:
  * $Log: not supported by cvs2svn $
+ * Revision 1.1.1.1  2004/11/09 16:18:56  trey
+ * imported hsvi into new repository
+ *
  * Revision 1.3  2003/07/16 16:12:44  trey
  * added support for terminal states
  *
