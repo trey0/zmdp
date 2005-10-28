02f86c6162f6a019890f6823f2e51ee2c5e00d48
Trey Smith <trey.smith@gmail.com>
2005-10-28 03:05:27 +0000

    added copyright header
    git-svn-id: file:///Users/mfsmith3/sandbox/cvs2svn/svnrepo/trunk/src@152 3bbcab3b-a713-480a-a40a-1fdaa183d0c8

--- parsers/pomdpCassandraWrapper.cc
+++ parsers/pomdpCassandraWrapper.cc
@@ -1,12 +1,42 @@
 /********** tell emacs we use -*- c++ -*- style comments *******************
- * $Revision: 1.2 $  $Author: trey $  $Date: 2005-10-27 21:34:20 $
- *  
- * @file    pomdpCassandraWrapper.cc
- * @brief   A wrapper that provides access to the pomdp read in by
- *          Tony Cassandra's file reading code, without requiring you
- *          to (1) directly reference global variables or (2) include
- *          all of the headers like sparse-matrix.h.  Also uses notation
- *          I am more familiar with.
+ $Revision: 1.3 $  $Author: trey $  $Date: 2005-10-28 03:05:27 $
+  
+ @file    pomdpCassandraWrapper.cc
+ @brief   A wrapper that provides access to the pomdp read in by
+          Tony Cassandra's file reading code, without requiring you
+          to (1) directly reference global variables or (2) include
+          all of the headers like sparse-matrix.h.  Also uses notation
+          I am more familiar with.
+
+ Copyright (c) 2002-2005, Trey Smith
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are
+ met:
+
+ * This code and derivatives may not be incorporated into commercial
+   products without specific prior written permission.
+ * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+ * The name of Trey Smith may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
  ***************************************************************************/
 
 /***************************************************************************
@@ -69,6 +99,9 @@ void PomdpCassandraWrapper::readFromFile(const string& fileName) {
 /***************************************************************************
  * REVISION HISTORY:
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2005/10/27 21:34:20  trey
+ * continued renaming process
+ *
  * Revision 1.1  2005/10/27 21:27:10  trey
  * renamed pomdp to pomdpCassandraWrapper
  *
