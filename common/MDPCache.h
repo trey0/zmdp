454898d2a0f8a045895582dcc9c9fbb91ceb5b1a
Trey Smith <trey.smith@gmail.com>
2006-02-13 21:47:53 +0000

    added prio field in MDPNode
    git-svn-id: file:///Users/mfsmith3/sandbox/cvs2svn/svnrepo/trunk/src@299 3bbcab3b-a713-480a-a40a-1fdaa183d0c8

--- common/MDPCache.h
+++ common/MDPCache.h
@@ -1,5 +1,5 @@
 /********** tell emacs we use -*- c++ -*- style comments *******************
- $Revision: 1.2 $  $Author: trey $  $Date: 2006-02-13 19:07:22 $
+ $Revision: 1.3 $  $Author: trey $  $Date: 2006-02-13 21:47:53 $
    
  @file    MDPCache.h
  @brief   Data structures for caching the explicit search graph
@@ -61,6 +61,7 @@ struct MDPNode {
   std::vector<MDPQEntry> Q;
   double lbVal, ubVal;
   bool isSolved; // used by LRTDP
+  double prio; // used by FRTDP
 
   bool isFringe(void) const { return Q.empty(); }
   size_t getNumActions(void) const { return Q.size(); }
@@ -76,6 +77,9 @@ typedef EXT_NAMESPACE::hash_map<std::string, MDPNode*> MDPHash;
 /***************************************************************************
  * REVISION HISTORY:
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2006/02/13 19:07:22  trey
+ * added MDPNode::getNextState() convenience method
+ *
  * Revision 1.1  2006/02/08 19:21:44  trey
  * initial check-in
  *
