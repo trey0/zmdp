e372149488e3d2cb21fa0c03f40625ac3b490277
Trey Smith <trey.smith@gmail.com>
2006-04-04 17:26:29 +0000

    moved OBS_IS_ZERO_EPS to MDP.h because it is used all over
    git-svn-id: file:///Users/mfsmith3/sandbox/cvs2svn/svnrepo/trunk/src@374 3bbcab3b-a713-480a-a40a-1fdaa183d0c8

--- common/MDP.h
+++ common/MDP.h
@@ -1,5 +1,5 @@
 /********** tell emacs we use -*- c++ -*- style comments *******************
- $Revision: 1.4 $  $Author: trey $  $Date: 2006-02-06 19:29:23 $
+ $Revision: 1.5 $  $Author: trey $  $Date: 2006-04-04 17:26:29 $
    
  @file    MDP.h
  @brief   No brief
@@ -36,6 +36,8 @@
 #include "zmdpCommonTypes.h"
 
 using namespace sla;
+
+#define OBS_IS_ZERO_EPS (1e-10)
 
 namespace zmdp {
 
@@ -93,6 +95,9 @@ struct MDP {
 /***************************************************************************
  * REVISION HISTORY:
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  2006/02/06 19:29:23  trey
+ * removed numOutcomes field; some MDPs have a varying number of outcomes depending on state
+ *
  * Revision 1.3  2006/02/01 01:09:37  trey
  * renamed pomdp namespace -> zmdp
  *
