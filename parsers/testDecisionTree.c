f7eb72359688d67915b3991ee354203ec0a3772e
Trey Smith <trey.smith@gmail.com>
2006-05-29 05:02:52 +0000

    initial check-in
    git-svn-id: file:///Users/mfsmith3/sandbox/cvs2svn/svnrepo/trunk/src@499 3bbcab3b-a713-480a-a40a-1fdaa183d0c8

---
+++ parsers/testDecisionTree.c
@@ -0,0 +1,68 @@
+/********** tell emacs we use -*- c++ -*- style comments *******************
+ $Revision: 1.1 $  $Author: trey $  $Date: 2006-05-29 05:02:52 $
+   
+ @file    testDecisionTree.c
+ @brief   No brief
+
+ Copyright (c) 2006, Trey Smith. All rights reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may
+ not use this file except in compliance with the License.  You may
+ obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ implied.  See the License for the specific language governing
+ permissions and limitations under the License.
+
+ ***************************************************************************/
+
+#include <stdio.h>
+
+#include "decision-tree.h"
+
+int main(int argc, char** argv)
+{
+  double result;
+
+  /* set up table */
+  dtInit(5, 5, 5);
+
+  dtAdd(0, 1, 2, 3, 0.5);
+  dtAdd(0, 1, 2, 3, 0.7);
+
+  dtAdd(0, 1, 3, -1, -10.1);
+  dtAdd(0, 1, 3, -1, -10.3);
+
+  dtAdd(1, 0, 0,  -1, 6.0);
+  dtAdd(1, 0, -1, -1, 7.0);
+
+  dtDebugPrint("decision tree:");
+
+  /* do a few queries */
+  result = dtGet(0, 1, 2, 3);
+  printf("expecting: result=%lf\n", 0.7);
+  printf("got:       result=%lf\n", result);
+
+  result = dtGet(0, 1, 3, 1);
+  printf("expecting: result=%lf\n", -10.3);
+  printf("got:       result=%lf\n", result);
+
+  result = dtGet(1, 0, 0, 0);
+  printf("expecting: result=%lf\n", 7.0);
+  printf("got:       result=%lf\n", result);
+
+  /* clean up */
+  dtDeallocate();
+
+  return 0;
+}
+
+/***************************************************************************
+ * REVISION HISTORY:
+ * $Log: not supported by cvs2svn $
+ *
+ ***************************************************************************/
