73407cbb056fa24cc0060a4ca2203b07fdb988ac
Trey Smith <trey.smith@gmail.com>
2006-06-14 00:22:40 +0000

    fixed printf format warning
    git-svn-id: file:///Users/mfsmith3/sandbox/cvs2svn/svnrepo/trunk/src@534 3bbcab3b-a713-480a-a40a-1fdaa183d0c8

--- search/HDP.cc
+++ search/HDP.cc
@@ -1,5 +1,5 @@
 /********** tell emacs we use -*- c++ -*- style comments *******************
- $Revision: 1.10 $  $Author: trey $  $Date: 2006-05-20 03:50:54 $
+ $Revision: 1.11 $  $Author: trey $  $Date: 2006-06-14 00:22:40 $
    
  @file    HDP.cc
  @brief   Implementation of Bonet and Geffner's HDP algorithm.
@@ -183,7 +183,7 @@ bool HDP::trialRecurse(MDPNode& cn, int depth)
 
   // try to label
   else if (getIdx(cn) == getLow(cn)) {
-    printf("  marking %lu nodes solved\n", nodeStack.size());
+    printf("  marking %d nodes solved\n", (int)nodeStack.size());
     while (nodeStack.top() != &cn) {
       MDPNode& sn = *nodeStack.pop();
       getIsSolved(sn) = true;
@@ -231,6 +231,9 @@ void HDP::derivedClassInit(void)
 /***************************************************************************
  * REVISION HISTORY:
  * $Log: not supported by cvs2svn $
+ * Revision 1.10  2006/05/20 03:50:54  trey
+ * fixed printf format to avoid warning
+ *
  * Revision 1.9  2006/04/28 17:57:41  trey
  * changed to use apache license
  *
