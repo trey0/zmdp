a701da4adf134d531bb7bd20e2fa820be93aea24
Trey Smith <trey.smith@gmail.com>
2006-11-09 20:46:28 +0000

    first working version
    git-svn-id: file:///Users/mfsmith3/sandbox/cvs2svn/svnrepo/trunk/src@821 3bbcab3b-a713-480a-a40a-1fdaa183d0c8

--- mdps/GenericDiscreteMDP.h
+++ mdps/GenericDiscreteMDP.h
@@ -1,10 +1,10 @@
 /********** tell emacs we use -*- c++ -*- style comments *******************
- $Revision: 1.1 $  $Author: trey $  $Date: 2006-11-08 16:47:04 $
+ $Revision: 1.2 $  $Author: trey $  $Date: 2006-11-09 20:46:28 $
    
  @file    GenericDiscreteMDP.h
  @brief   No brief
 
- Copyright (c) 2002-2005, Trey Smith. All rights reserved.
+ Copyright (c) 2006, Trey Smith.
 
  Licensed under the Apache License, Version 2.0 (the "License"); you may
  not use this file except in compliance with the License.  You may
@@ -36,53 +36,30 @@ using namespace sla;
 namespace zmdp {
 
 struct GenericDiscreteMDP : public CassandraModel {
-  GenericDiscreteMDP(void) {}
-  GenericDiscreteMDP(const std::string& fileName, bool useFastModelParser = false,
-		     int _maxHorizon = -1);
+  bool boundsInitialized;
+  double globalLowerBound;
+  double globalUpperBound;
 
-  void readFromFile(const std::string& fileName,
-		    bool useFastModelParser = false);
-
-  // returns the initial belief
-  const belief_vector& getInitialBelief(void) const;
-
-  // sets result to be the vector of observation probabilities when from
-  // belief b action a is selected
-  obs_prob_vector& getObsProbVector(obs_prob_vector& result, const belief_vector& b,
-				    int a) const;
-
-  // sets result to be the next belief when from belief b action a is
-  // selected and observation o is observed
-  belief_vector& getNextBelief(belief_vector& result, const belief_vector& b,
-			       int a, int o) const;
+  GenericDiscreteMDP(const std::string& fileName,
+		     const ZMDPConfig* _config);
 
   // returns the expected immediate reward when from belief b action a is selected
   double getReward(const belief_vector& b, int a) const;
 
+  // POMDP-as-belief-MDP implementations for virtual functions declared in MDP
+  const state_vector& getInitialState(void) const;
+  bool getIsTerminalState(const state_vector& s) const;
+  outcome_prob_vector& getOutcomeProbVector(outcome_prob_vector& result,
+					    const state_vector& b,
+					    int a) const;
+  state_vector& getNextState(state_vector& result,
+			     const state_vector& s,
+			     int a, int o) const;
+
+  double getLongTermFactor(void) const;
+
   AbstractBound* newLowerBound(const ZMDPConfig* _config) const;
   AbstractBound* newUpperBound(const ZMDPConfig* _config) const;
-
-  // POMDP-as-belief-MDP aliases for functions implemented in MDP
-  int getBeliefSize(void) const { return getNumStateDimensions(); }
-  int getNumObservations(void) const { return numObservations; }
-  void setBeliefSize(int beliefSize) { numStateDimensions = beliefSize; }
-  void setNumObservations(int _numObservations) { numObservations = _numObservations; }
-
-  // POMDP-as-belief-MDP implementations for virtual functions declared in MDP
-  const state_vector& getInitialState(void) const { return getInitialBelief(); }
-  bool getIsTerminalState(const state_vector& s) const;
-  outcome_prob_vector& getOutcomeProbVector(outcome_prob_vector& result, const state_vector& b,
-					    int a) const
-    { return getObsProbVector(result,b,a); }
-  state_vector& getNextState(state_vector& result, const state_vector& s,
-			     int a, int o) const
-    { return getNextBelief(result,s,a,o); }
-  
-protected:
-  void readFromFileCassandra(const std::string& fileName);
-  void readFromFileFast(const std::string& fileName);
-
-  void debugDensity(void);
 };
 
 }; // namespace zmdp
@@ -92,6 +69,9 @@ protected:
 /***************************************************************************
  * REVISION HISTORY:
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2006/11/08 16:47:04  trey
+ * initial check-in
+ *
  *
  ***************************************************************************/
 
