5759f3425eec11b743010265b2784df79cf10b68
Trey Smith <trey.smith@gmail.com>
2007-03-24 22:44:15 +0000

    initial check-in
    git-svn-id: file:///Users/mfsmith3/sandbox/cvs2svn/svnrepo/trunk/src@942 3bbcab3b-a713-480a-a40a-1fdaa183d0c8

---
+++ mdps/CacheMDP.cc
@@ -0,0 +1,187 @@
+/********** tell emacs we use -*- c++ -*- style comments *******************
+ $Revision: 1.1 $  $Author: trey $  $Date: 2007-03-24 22:44:15 $
+  
+ @file    CacheMDP.cc
+ @brief   No brief
+
+ Copyright (c) 2007, Trey Smith.
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may
+ not use this file except in compliance with the License.  You may
+ obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ implied.  See the License for the specific language governing
+ permissions and limitations under the License.
+
+ ***************************************************************************/
+
+/***************************************************************************
+ * INCLUDES
+ ***************************************************************************/
+
+#include <assert.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <getopt.h>
+#include <errno.h>
+
+#include <iostream>
+#include <fstream>
+#include <map>
+
+#include "MatrixUtils.h"
+#include "Solver.h"
+#include "CacheMDP.h"
+
+using namespace std;
+using namespace MatrixUtils;
+
+namespace zmdp {
+
+#define ACTION_MOVE_LEFT (0)
+#define ACTION_MOVE_RIGHT (1)
+
+CacheMDP::CacheMDP(MDP* _problem) :
+  MDP(*_problem),
+  problem(_problem)
+{
+  root = getNodeX(problem->getInitialState());
+  initialSI.resize(1);
+  initialSI.push_back(0, root->si);
+}
+
+CacheMDP::~CacheMDP(void)
+{
+  // do not delete 'problem', which is owned by the caller
+  // do not delete 'root', which is owned by the node table
+
+  // deallocate the states in the node table
+  FOR_EACH (elt, nodeTable) {
+    delete (*elt);
+  }
+}
+
+const state_vector& CacheMDP::getInitialState(void)
+{
+  return initialSI;
+}
+
+bool CacheMDP::getIsTerminalState(const state_vector& s)
+{
+  return getNode(s)->isTerminal;
+}
+
+outcome_prob_vector& CacheMDP::getOutcomeProbVector(outcome_prob_vector& result,
+						    const state_vector& s, int a)
+{
+  result = getQ(*getNode(s), a)->opv;
+  return result;
+}
+
+state_vector& CacheMDP::getNextState(state_vector& result, const state_vector& s,
+				     int a, int o)
+{
+  int spi = getQ(*getNode(s), a)->outcomes[o]->nextState->si;
+
+#if 0
+  int si = (int) (s(0));
+  printf("getNextState si=%d a=%d o=%d spi=%d\n",
+	 si, a, o, spi);
+  static int cnt = 0;
+  if (cnt++ > 1000) exit(0);
+#endif
+
+  result.resize(1);
+  result.push_back(0, spi);
+  return result;
+}
+
+double CacheMDP::getReward(const state_vector& s, int a)
+{
+  assert(s.size() == 1 && s.data.size() == 1);
+
+  double reward = getQ(*getNode(s), a)->immediateReward;
+
+#if 0
+  int si = (int) (s(0));
+  printf("getReward si=%d a=%d r=%.1lf\n", si, a, reward);
+#endif
+
+  return reward;
+}
+
+const state_vector& CacheMDP::translateState(state_vector& result, const state_vector& s)
+{
+  result = getNode(s)->s;
+  return result;
+}
+
+CMDPNode* CacheMDP::getNode(const state_vector& s)
+{
+  assert(s.size() == 1 && s.data.size() == 1);
+  int si = ((int) s(0));
+  return nodeTable[si];
+}
+
+CMDPNode* CacheMDP::getNodeX(const state_vector& s)
+{
+  string hs = hashable(s);
+  CMDPHash::iterator pr = lookup.find(hs);
+  if (lookup.end() == pr) {
+    // create a new fringe node
+    int si = nodeTable.size();
+    CMDPNode& cn = *(new CMDPNode);
+    cn.s = s;
+    cn.si = si;
+    cn.isTerminal = problem->getIsTerminalState(s);
+    cn.Q.resize(problem->getNumActions(), NULL);
+    cn.userData = NULL;
+    nodeTable.push_back(&cn);
+    lookup[hs] = si;
+    return &cn;
+  } else {
+    // return existing node
+    return nodeTable[pr->second];
+  }
+}
+
+CMDPQEntry* CacheMDP::getQ(CMDPNode& cn, int a)
+{
+  if (NULL == cn.Q[a]) {
+    CMDPQEntry& Qa = *(new CMDPQEntry);
+    Qa.immediateReward = problem->getReward(cn.s, a);
+    problem->getOutcomeProbVector(Qa.opv, cn.s, a);
+    Qa.outcomes.resize(Qa.opv.size(), NULL);
+    FOR (o, Qa.opv.size()) {
+      if (Qa.opv(o) > OBS_IS_ZERO_EPS) {
+	CMDPEdge* e = new CMDPEdge;
+	state_vector sp;
+	problem->getNextState(sp, cn.s, a, o);
+	e->nextState = getNodeX(sp);
+	e->userData = NULL;
+	Qa.outcomes[o] = e;
+      } else {
+	Qa.outcomes[o] = NULL;
+      }
+    }
+    cn.Q[a] = &Qa;
+    return &Qa;
+  } else {
+    return cn.Q[a];
+  }
+}
+
+}; // namespace zmdp
+
+/***************************************************************************
+ * REVISION HISTORY:
+ * $Log: not supported by cvs2svn $
+ *
+ ***************************************************************************/
---
+++ mdps/CacheMDP.h
@@ -0,0 +1,122 @@
+/********** tell emacs we use -*- c++ -*- style comments *******************
+ $Revision: 1.1 $  $Author: trey $  $Date: 2007-03-24 22:44:15 $
+   
+ @file    CacheMDP.h
+ @brief   No brief
+
+ Copyright (c) 2007, Trey Smith.
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may
+ not use this file except in compliance with the License.  You may
+ obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ implied.  See the License for the specific language governing
+ permissions and limitations under the License.
+
+ ***************************************************************************/
+
+#ifndef INCCacheMDP_h
+#define INCCacheMDP_h
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "zmdpConfig.h"
+#include "MDPModel.h"
+#include "AbstractBound.h"
+
+namespace zmdp {
+
+struct CMDPNode;
+
+struct CMDPUserData {
+  virtual ~CMDPUserData(void) {}
+};
+
+struct CMDPEdge {
+  CMDPNode* nextState;
+  // code that uses CacheMDP can associate arbitrary extra data with an edge
+  CMDPUserData* userData;
+
+  ~CMDPEdge(void) {
+    // do not delete nextState, which is owned by the global node table
+    if (NULL != userData) delete userData;
+  }
+};
+
+struct CMDPQEntry {
+  double immediateReward;
+  outcome_prob_vector opv;
+  std::vector<CMDPEdge*> outcomes;
+
+  size_t getNumOutcomes(void) const { return outcomes.size(); }
+};
+
+struct CMDPNode {
+  state_vector s;
+  int si;
+  bool isTerminal;
+  std::vector<CMDPQEntry*> Q;
+  // code that uses CacheMDP can associate arbitrary extra data with a node
+  CMDPUserData* userData;
+
+  ~CMDPNode(void) {
+    FOR_EACH (elt, Q) {
+      if (NULL != (*elt)) {
+	delete (*elt);
+      }
+    }
+    if (NULL != userData) delete userData;
+  }
+  bool isFringe(void) const { return Q.empty(); }
+  size_t getNumActions(void) const { return Q.size(); }
+};
+
+typedef EXT_NAMESPACE::hash_map<std::string, int> CMDPHash;
+typedef std::vector<CMDPNode*> CMDPNodeTable;
+
+struct CacheMDP : public MDP {
+  MDP* problem;
+  CMDPNode* root;
+  CMDPHash lookup;
+  CMDPNodeTable nodeTable;
+  state_vector initialSI;
+  
+  CacheMDP(MDP* _problem);
+  ~CacheMDP(void);
+  
+  const state_vector& getInitialState(void);
+  bool getIsTerminalState(const state_vector& s);
+  outcome_prob_vector& getOutcomeProbVector(outcome_prob_vector& result,
+					    const state_vector& s, int a);
+  state_vector& getNextState(state_vector& result, const state_vector& s, int a,
+			     int o);
+  double getReward(const state_vector& s, int a);
+
+  const state_vector& translateState(state_vector& result, const state_vector& s);
+
+  AbstractBound* newLowerBound(const ZMDPConfig* _config) { assert(0); }
+  AbstractBound* newUpperBound(const ZMDPConfig* _config) { assert(0); }
+
+  CMDPNode* getNode(const state_vector& s);
+  CMDPQEntry* getQ(CMDPNode& cn, int a);
+  CMDPNode* getNodeX(const state_vector& s);
+
+};
+
+}; // namespace zmdp
+
+#endif // INCCacheMDP_h
+
+/***************************************************************************
+ * REVISION HISTORY:
+ * $Log: not supported by cvs2svn $
+ *
+ ***************************************************************************/
+
